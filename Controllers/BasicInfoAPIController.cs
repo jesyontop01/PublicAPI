45a54ae55e3394c492958eae19ef74cf6c18e044
Adelaja Jeremiah Anuoluwapo <31852242+jesyontop01@users.noreply.github.com>
2025-02-02 16:56:18 +0100

    Add project files.

---
+++ Controllers/BasicInfoAPIController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PublicAPI.Dtos;
+using System.Text.Json;
+
+namespace PublicAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasicInfoAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            BasicInfoDto BasicIfo = new BasicInfoDto();
+            BasicIfo.GithubUrl = "https://github.com/jesyontop01/HNGPublicAPI";
+            BasicIfo.Email = "jesyontop01@gmail.com";
+            DateTime currentDate = DateTime.Now;
+
+            BasicIfo.CurrentDatetime = currentDate.ToUniversalTime().ToString("u").Replace(" ", "T");
+
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower };
+            var json = JsonSerializer.Serialize(BasicIfo, options);
+
+            return Ok(json);
+
+        }
+    }
+}
---
+++ Dtos/BasicInfoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PublicAPI.Dtos
+{
+    public class BasicInfoDto
+    {
+        public string Email { get; set; }
+        public string CurrentDatetime { get; set; }
+        public string GithubUrl { get; set; }
+
+    }
+}
